b91e1a6a0ef1d9534e39916b8ac43b791032839a
rameem2003 <rameem2019@gmail.com>
2025-04-11 06:39:01 +0600

    responsive

--- src/components/resuseable/Speciality.jsx
+++ src/components/resuseable/Speciality.jsx
@@ -9,7 +9,7 @@ const Speciality = () => {
     <section className=" py-[50px] 2xl:py-[100px]">
       <Container>
         <Flex className="items-center flex-col lg:flex-row">
-          <div className=" w-full lg:w-1/2 relative">
+          <div className=" w-full xl:w-7/12 2xl:w-1/2 relative">
             <div className="w-[300px] h-[300px] md:w-[622px] md:h-[662px] max-w-[662px] rounded-full bg-[#0078FF] blur-[320px] top-[50px] left-0 absolute z-[-1]"></div>
             <h2 className="text-3xl md:text-4xl xl:text-4xl 2xl:text-[46px] text-white font-bold w-auto  xl:w-[745px] capitalize">
               Why <span className=" text-[#0078FF]">ROL Studio Bangladesh</span>{" "}
@@ -20,7 +20,7 @@ const Speciality = () => {
               {speciality.map((item, i) => (
                 <p
                   key={i}
-                  className=" font-medium text-lg xl:text-xl 2xl:text-[26px] text-white w-full sm:w-1/2 mb-9"
+                  className=" font-medium text-lg xl:text-2xl 2xl:text-[24px] text-white w-full sm:w-1/2 mb-9"
                 >
                   <span className=" text-[#0078FF]">{item.id}. </span>
                   {item.title}
@@ -28,7 +28,7 @@ const Speciality = () => {
               ))}
             </div>
           </div>
-          <div className=" w-full lg:w-1/2">
+          <div className=" w-full xl:w-5/12 2xl:w-1/2">
             <div className=" relative z-[100]">
               <div className="  w-[100px] xl:w-[346px] h-[100px] xl:h-[346px] bg-[#0C3869] blur-[75px] absolute top-[-80px] left-0 z-[-1]"></div>
               <div className="  w-[100px] xl:w-[200px] h-[100px] xl:h-[200px] bg-[#5F7184] blur-[75px] absolute bottom-[-60px] right-0 z-[-1]"></div>
